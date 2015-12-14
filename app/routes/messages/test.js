addce00d9f6489c2bb3d98f8c56d237e86cc036c
Anna Karlsson <anna.karlsson@omegapoint.se>
2015-12-14 19:08:44 +0100

    a href --> link-to

--- app/routes/messages/test.js
+++ app/routes/messages/test.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        return this.store.filter('message', function(message) {
+            return message.get('room') === "test";
+        });
+    },
+    renderTemplate(controller, model) {
+        this.render('messages.index', {
+            model: model
+        });
+    }
 });
