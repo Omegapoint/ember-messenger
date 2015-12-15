a2130d1f66d7cc362a41ad878dcb7d4d6bdd5e91
Anna Karlsson <anna.karlsson@omegapoint.se>
2015-12-15 14:35:22 +0100

    author for chat added

--- app/components/author-input.js
+++ app/components/author-input.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     updateAuthor(author) {
+      author = author.replace(/\W/g, '').trim();
       this.sendAction('action', author);
     }
   }
--- app/components/message-input.js
+++ app/components/message-input.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
     actions: {
         submitMessage(newMessage) {
             if (newMessage) {
+                console.log("ROOM? ");
                 this.sendAction('action', newMessage);
             }
             this.set('newMessage', '');
