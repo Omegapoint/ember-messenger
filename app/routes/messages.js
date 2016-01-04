9e57db9bfcb46d40fe71f0d5ccce653020ddacb0
Anna Karlsson <anna.karlsson@omegapoint.se>
2016-01-04 13:52:36 +0100

    la till rum i newMessage, antagligen inte på bästa sätt (kommer inte på något bättre just nu)

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -1,18 +1,17 @@
 import Ember from 'ember';
 
-var inRoom = 'test';
 var localAuthor = 'anonymous';
+
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('message');
     },
-
     actions: {
         createMessage(newMessage) {
            this.store.createRecord('message', {
                message: newMessage,
                timestamp: new Date(),
-               room: inRoom,
+               room: this.controllerFor("application").get("currentPath").substring(9),
                speaker: localAuthor
            }).save();
         },
