858c3c8752472aa177f336ff22221d5826a189d4
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-01-10 22:08:41 +0100

    Made author input work with the new model

--- app/controllers/room.js
+++ app/controllers/room.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  rooms: Ember.inject.controller(),
+
   actions: {
     createMessage(newMessage) {
       let record = this.store.createRecord('message', {
           message: newMessage,
           timestamp: new Date(),
           room: this.model.id,
-          speaker: "me"
+          speaker: this.get('rooms').get('userName')
       });
       this.model.get('messages').addObject(record);
       this.model.save();
---
+++ app/controllers/rooms.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  userName: "anonymous",
+
+  actions: {
+    updateAuthor(newUserName) {
+      this.set('userName', newUserName);
+    }
+  }
+});
