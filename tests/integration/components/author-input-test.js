d8cc4e1eabdb474e6e97acf9838d736c17cde8d0
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-01-17 14:45:33 +0100

    Added test that component renders an input element

--- tests/integration/components/author-input-test.js
+++ tests/integration/components/author-input-test.js
@@ -5,9 +5,10 @@ moduleForComponent('author-input', 'Integration | Component | author input', {
   integration: true
 });
 
-test('it a Handle label and an input element', function(assert) {
+test('it renders a Handle label and an input element', function(assert) {
 
   this.render(hbs`{{author-input}}`);
 
-  assert.equal(this.$().text().trim(), 'Handle:');
+  assert.equal(this.$().text().trim(), 'Handle:', "It renders a 'Handle' label.");
+  assert.equal(this.$("input").length, 1, "It renders one input element.");
 });
