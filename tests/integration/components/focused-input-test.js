cbeaf16ac56dcbb3614dda53c787c59f8cc46b5b
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-01-17 21:13:36 +0100

    Added test for focused input

--- tests/integration/components/focused-input-test.js
+++ tests/integration/components/focused-input-test.js
@@ -5,21 +5,8 @@ moduleForComponent('focused-input', 'Integration | Component | focused input', {
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
-
+test('it renders an input element', function(assert) {
   this.render(hbs`{{focused-input}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#focused-input}}
-      template block text
-    {{/focused-input}}
-  `);
-
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$("input").length, 1, 'It renders one input');
 });
