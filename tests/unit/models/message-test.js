776e004eeed6a6a4a314db5cd4f8e80189c22f9a
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-01-17 09:55:10 +0100

    Added a test for message attributes

--- tests/unit/models/message-test.js
+++ tests/unit/models/message-test.js
@@ -6,6 +6,23 @@ moduleForModel('message', 'Unit | Model | message', {
   needs: [ 'model:room']
 });
 
+test('it has message, author, and time fields', function(assert) {
+  let messageModel = this.store().modelFor('message');
+  let attributes = Ember.get(messageModel, 'attributes');
+
+  let message = attributes.get('message');
+  assert.ok(message, "Model has a message attribute.");
+  assert.equal(message.type, 'string', "Message attribute has string type.");
+
+  let author = attributes.get('speaker');
+  assert.ok(author, "Model has an author attribute.");
+  assert.equal(author.type, 'string', "Author attribute has string type.");
+
+  let timestamp = attributes.get('timestamp');
+  assert.ok(timestamp, "Model has a timestamp attribute.");
+  assert.equal(timestamp.type, 'date', "Timestamp attribute has date type.");
+});
+
 test('it belongs to a room', function(assert) {
   let messageModel = this.store().modelFor('message');
   let relations = Ember.get(messageModel, 'relationshipsByName');
