cc7dfac9f63673ce0089d120b645487efa5c06ff
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-01-17 18:01:53 +0100

    Now loading all messages for a room

---
+++ app/routes/rooms/room.js
@@ -0,0 +1,6 @@
+export default Ember.Route.extend({
+  model(params) {
+    this.store.findAll('message'); // these won't load automatically, don't know why.
+    return this.store.find('room', params.room_id);
+  }
+});
