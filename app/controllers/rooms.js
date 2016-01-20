c20e0bdd758d07997307648e20622a153cef6296
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-01-20 21:32:43 +0100

    Final exercise: create new room

--- app/controllers/rooms.js
+++ app/controllers/rooms.js
@@ -6,17 +6,6 @@ export default Ember.Controller.extend({
   actions: {
     updateAuthor(newUserName) {
       this.set('userName', newUserName);
-    },
-    createRoom(){
-      let room = prompt('Please enter room name\n(max 15 character, no spaces)', '');
-      if (room !== null || room === '') {
-          room = room.replace(/\s/g, '').substring(0,15);
-          let record = this.store.createRecord('room', {
-              id: room,
-              name: room
-          });
-          record.save();
-      }
     }
   }
 });
---
+++ exercise.md
@@ -0,0 +1,5 @@
+# Exercise: Make it possible to add a new room.
+
+How it's done is up to you! We do however recommend writing an acceptance test for this feature.
+
+Hint: Use the little '+' as a starting point.
