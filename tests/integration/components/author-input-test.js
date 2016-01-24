becea336ed57ca0b52abe59f7581bc32e3824c06
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2016-01-24 13:49:49 +0100

    Changed author input label from Handle: to Your name:

--- tests/integration/components/author-input-test.js
+++ tests/integration/components/author-input-test.js
@@ -5,10 +5,10 @@ moduleForComponent('author-input', 'Integration | Component | author input', {
   integration: true
 });
 
-test('it renders a Handle label and an input element', function(assert) {
+test('it renders a "Your name:" label and an input element', function(assert) {
 
   this.render(hbs`{{author-input}}`);
 
-  assert.equal(this.$().text().trim(), 'Handle:', "It renders a 'Handle' label.");
+  assert.equal(this.$().text().trim(), 'Your name:', "It renders a 'Handle' label.");
   assert.equal(this.$("input").length, 1, "It renders one input element.");
 });
